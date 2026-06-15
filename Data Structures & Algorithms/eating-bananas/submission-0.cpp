86417e465572d396a1315ff44f458ebed70a0040
Sachin Shakya <sachinshakya613@gmail.com>
2026-06-15 17:21:41 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size(),mid,ans;
+        int sum=0;
+        int end=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=piles[i];
+            end=max(end,piles[i]);
+            
+        }
+        int start = sum/h;
+
+        if(!start)
+        start=1;
+
+        while(start<=end)
+        {
+            mid = start + (end-start)/2;
+            int total_time=0;
+
+            for(int i=0;i<n;i++)
+            {
+                total_time+=piles[i]/mid;
+                if(piles[i]%mid)
+                total_time++;
+            }
+
+            if(total_time>h)
+            start = mid + 1;
+            else
+            {
+                ans = mid;
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
